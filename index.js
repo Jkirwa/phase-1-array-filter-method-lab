3881fafbe5e118aa21d45e678b59aada87cb09b5
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-10 10:29:33 +0300

    commit tree 88c46992f7f24c0dfa8079206cadea69f369d42e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+
+function findMatching(myArray, string) {
+    return myArray.filter(
+      (matchingName) => matchingName.toUpperCase() === string.toUpperCase()
+    );
+  }
+  function fuzzyMatch(myArray, myString) {
+    return myArray.filter(
+      (matchingName) =>
+        matchingName.toUpperCase().indexOf(myString.toUpperCase()) === 0
+    );
+  }
+  function matchName(myArray, driverString) {
+    return myArray.filter((record) => record.name === driverString);
+  }
